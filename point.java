4bbed29dbe3cd41f64c625f493d7c9e9e155b3ae
khadijaidboujnane <idboujnane.fst@uhp.ac.ma>
2023-12-13 16:57:06 +0100

    ex2td2

--- point.java
+++ point.java
@@ -1,33 +1,44 @@
 public class point {
-
-    private char nom ;
-    private double abs ;
-
-    // constrcteur
-    public point(char nom, double abs) {
-        this.nom = nom;
-        this.abs = abs;
+    private double x;
+    private double y;
+    //constructeur
+    public point(){
+        this.x=0;
+        this.y=0;
     }
-    // methode affichage
-    public void affiche(){
-        System.out.println("point=  "+nom+"abscisse=  "+abs);
 
-
-
+    public point(double x ,double y){
+        this.x=x;
+        this.y=y;
     }
-    //methode de translation
-    public void translate(double x){
-        abs+=x;
+    //getter and setter
+
+
+    public double getx() {
+        return x;
     }
-    public static void main(String[] args) {
-        point pointA=new point('A',2);
-        System.out.println(" point A avant translation :   ");
-        pointA.affiche();
-        pointA.translate(3);
-        System.out.println(" point A apres translation :   ");
-        pointA.affiche();
 
+    public double getY() {
+        return y;
+    }
 
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+    public double norme(){
+        return Math.sqrt(x*x+y*y);
+    }
+    public static void main(String[] args){
+        point pointB=new point();
+        System.out.println("les coordonnee du point B est ( "+pointB.getx()+","+ pointB.getY()+")");
+        pointB.setX(2);
+        pointB.setY(3);
+        System.out.println("les  nouvelles coordonnee du point B est ( "+pointB.getx()+","+ pointB.getY()+")");
+        System.out.println(" la norme du point B est :   "+pointB.norme());
 
     }
 }
