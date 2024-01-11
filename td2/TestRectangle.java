25d642c191f7c69b321d13d8df6b821f0989103d
khadijaidboujnane <idboujnane.fst@uhp.ac.ma>
2024-01-11 20:27:59 +0000

    td2

---
+++ td2/TestRectangle.java
@@ -0,0 +1,27 @@
+package td2;
+
+public class TestRectangle {
+    public static void main(String[] args) {
+        // Test des constructeurs
+        Rectangle rectangle1 = new Rectangle();
+        Rectangle rectangle2 = new Rectangle(6.0, 4.0);
+        Rectangle rectangle3 = new Rectangle(rectangle2);
+
+        // Affichage des rectangles
+        System.out.println("Rectangle 1: " + rectangle1);
+        System.out.println("Rectangle 2: " + rectangle2);
+        System.out.println("Rectangle 3 (copie de Rectangle 2): " + rectangle3);
+
+        // Modification des attributs
+        rectangle1.setLongueur(4.0);
+        rectangle1.setLargeur(4.0);
+
+        // Affichage des attributs après modification
+        System.out.println("Rectangle 1 après modification: " + rectangle1);
+
+        // Calcul du périmètre, de l'aire et vérification si c'est un carré
+        System.out.println("Périmètre de Rectangle 2: " + rectangle2.perimetre());
+        System.out.println("Aire de Rectangle 2: " + rectangle2.aire());
+        System.out.println("Rectangle 2 est un carré : " + rectangle2.isCarre());
+    }
+}
---
+++ td2/point.java
@@ -0,0 +1,46 @@
+package td2;
+
+public class point {
+    private double x;
+    private double y;
+    //constructeur
+    public point(){
+        this.x=0;
+        this.y=0;
+    }
+
+    public point(double x , double y){
+        this.x=x;
+        this.y=y;
+    }
+    //getter and setter
+
+
+    public double getx() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+    public double norme(){
+        return Math.sqrt(x*x+y*y);
+    }
+    public static void main(String[] args){
+        point pointB=new point();
+        System.out.println("les coordonnee du point B est ( "+pointB.getx()+","+ pointB.getY()+")");
+        pointB.setX(2);
+        pointB.setY(3);
+        System.out.println("les  nouvelles coordonnee du point B est ( "+pointB.getx()+","+ pointB.getY()+")");
+        System.out.println(" la norme du point B est :   "+pointB.norme());
+
+    }
+}
